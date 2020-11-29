2681916eaa8c396851a83425da2002a9becaa1f1
lelislois Ben <samirbenzada@gmail.com>
2020-11-29 17:29:53 -0500

    updated

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from utils import APIException
+from models import db, Consultants
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 CORS(app)
 
 # Handle/serialize errors like a JSON object
@@ -13,6 +15,19 @@ def handle_invalid_usage(error):
 @app.route('/')
 def hello_world():
     return "<div style='text-align: center; background-color: orange'><h1>Backend running...</h1><br/><h3>Welcome back samir</h3><img src='https://media.gettyimages.com/photos/woman-sitting-by-washing-machine-picture-id117852649?s=2048x2048' width='80%' /></div>"
+
+@app.route('/consultant', methods=['GET'])
+def handle_consultant():
+
+    if request.method == 'GET':
+        consultant = Consultants.query.all()
+
+        if not consultant:
+            return jsonify({'msg':'User not found'}), 404
+
+        return jsonify( [x.serialize() for x in consultant] ), 200
+
+    return "Invalid Method", 404
 
 @app.route('/add-consultant')
 def new_consultant():
