bfae4b5d6b5917b59cad1a5e36a775be1cfdd77b
lelislois Ben <samirbenzada@gmail.com>
2020-11-30 00:25:22 -0500

    updated

--- src/main.py
+++ src/main.py
@@ -27,13 +27,26 @@ def hello_world():
 def handle_login():
     body = request.get_json()
 
-    if body['email'] = loginEmail & body['password'] = loginPassword:
+        if !body:
+            if body['email'] != loginEmail:
+                return jsonify({
+                    'msg': 'wrong email'
+                    })
+            elif body['password'] != loginPassword:
+                    return jsonify({
+                    'msg': 'wrong password'
+                    })
+            else:
+                return jsonify({
+                    'token': create_jwt(identity=1),
+                    'email': body['email'],
+                    'name': "admin"
+                    })
+        else:
+            return jsonify({
+                    'msg': 'login cannot be empty'
+                    })
 
-        return jsonify({
-              'token': create_jwt(identity=1),
-              'email': body['email'],
-              'name': "admin"
-              })
     return "Invalid Method", 404
 
 
