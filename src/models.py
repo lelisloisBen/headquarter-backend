901229275cbdf4b58e255d31858e3887d7359b32
samirbenzada <samirbenzada@gmail.com>
2020-12-23 14:35:20 -0500

    update

--- src/models.py
+++ src/models.py
@@ -141,6 +141,7 @@ class datavaultusers(db.Model):
     firstname = db.Column(db.String(120), nullable=False)
     lastname = db.Column(db.String(120),  nullable=False )
     password = db.Column(db.String(80), nullable=False)
+    courses = db.Column(db.Integer, nullable=False, default=0)
 
     def __repr__(self):
         return '<datavaultusers %r>' % self.email
@@ -150,5 +151,6 @@ class datavaultusers(db.Model):
             "id": self.id,
             "email": self.email,
             "firstname": self.firstname,
-            "lastname": self.lastname
+            "lastname": self.lastname,
+            "courses": self.courses
         }
